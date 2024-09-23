071ee5d6c993033d3ac6e435dc2acfd7f45172eb
Cristian Bucurenciu <bucurenciu.cristian@gmail.com>
2024-09-23 18:49:38 +0300

    feat: new alert via Delete Button.

--- react-native/easy/taskly/App.tsx
+++ react-native/easy/taskly/App.tsx
@@ -1,12 +1,47 @@
-import { StatusBar } from "expo-status-bar";
-import { PixelRatio, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    console.log("DELETING");
+    Alert.alert(
+      "Are you sure you want to delete?",
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("DELETED"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          onPress: () => console.log("NOT DELETED"),
+          style: "cancel",
+        },
+      ],
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         {/*PixelRatio.get()*/}
-        <Text style={styles.itemText}>Cofee</Text>
+
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleDelete}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -15,17 +50,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: theme.colorWhite,
     justifyContent: "center",
   },
   itemContainer: {
     borderBottomWidth: 1,
-    borderBottomColor: "#1a759f",
+    borderBottomColor: theme.colorCelurean,
     paddingHorizontal: 8,
     paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   itemText: {
     fontSize: 18,
-    fontWeight: 200,
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
