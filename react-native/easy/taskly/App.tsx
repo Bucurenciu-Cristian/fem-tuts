7d6d047ee18d3ff7ebb52efff205311e32001ca1
Cristian Bucurenciu <bucurenciu.cristian@gmail.com>
2024-09-23 18:11:37 +0300

    ESLINT + Prettier + minor style

--- react-native/easy/taskly/App.tsx
+++ react-native/easy/taskly/App.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { PixelRatio, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello there</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        {/*PixelRatio.get()*/}
+        <Text style={styles.itemText}>Cofee</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +15,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#1a759f",
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: 200,
   },
 });
