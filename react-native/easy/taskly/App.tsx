0b66fe1bc8a53daaeaecee5d63795a18cecd5234
Cristian Bucurenciu <bucurenciu.cristian@gmail.com>
2024-09-23 19:24:35 +0300

    feat: ShoppingListItem refactor and some styling.

--- react-native/easy/taskly/App.tsx
+++ react-native/easy/taskly/App.tsx
@@ -1,48 +1,13 @@
-import {
-  Alert,
-  Pressable,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
 import { theme } from "./theme";
+import { ShoppingListItem } from "./components/ShoppingListItem";
 
 export default function App() {
-  const handleDelete = () => {
-    console.log("DELETING");
-    Alert.alert(
-      "Are you sure you want to delete?",
-      "It will be gone for good",
-      [
-        {
-          text: "Yes",
-          onPress: () => console.log("DELETED"),
-          style: "destructive",
-        },
-        {
-          text: "Cancel",
-          onPress: () => console.log("NOT DELETED"),
-          style: "cancel",
-        },
-      ],
-    );
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        {/*PixelRatio.get()*/}
-
-        <Text style={styles.itemText}>Coffee</Text>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={handleDelete}
-          activeOpacity={0.8}
-        >
-          <Text style={styles.buttonText}>Delete</Text>
-        </TouchableOpacity>
-      </View>
+      <ShoppingListItem name="Coffee" />
+      <ShoppingListItem name="Tea" isCompleted />
+      <ShoppingListItem name="Milk" isCompleted />
     </View>
   );
 }
@@ -52,27 +17,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
-  },
-  itemContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: theme.colorCelurean,
-    paddingHorizontal: 8,
-    paddingVertical: 16,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  itemText: {
-    fontSize: 18,
-  },
-  button: {
-    backgroundColor: theme.colorBlack,
-    padding: 8,
-    borderRadius: 8,
-  },
-  buttonText: {
-    color: theme.colorWhite,
-    textTransform: "uppercase",
-    letterSpacing: 1,
   },
 });
---
+++ react-native/easy/taskly/components/ShoppingListItem.tsx
@@ -0,0 +1,93 @@
+import { Alert, StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import { theme } from "../theme";
+
+type Props = {
+  name: string;
+  isCompleted?: boolean;
+};
+
+export function ShoppingListItem({ name, isCompleted }: Props) {
+  const handleDelete = () => {
+    console.log("DELETING");
+    Alert.alert(
+      `Are you sure you want to delete ${name}?`,
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("DELETED"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          onPress: () => console.log("NOT DELETED"),
+          style: "cancel",
+        },
+      ],
+    );
+  };
+
+  return (
+    <View
+      style={[
+        styles.itemContainer,
+        isCompleted ? styles.completedContainer : undefined,
+      ]}
+    >
+      <Text
+        style={[
+          styles.itemText,
+          isCompleted ? styles.completedText : undefined,
+        ]}
+      >
+        {name}
+      </Text>
+      <TouchableOpacity
+        style={[
+          styles.button,
+          isCompleted ? styles.completedButton : undefined,
+        ]}
+        onPress={handleDelete}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.buttonText}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCelurean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
+  completedContainer: {
+    backgroundColor: theme.colorLightGrey,
+    borderBottomColor: theme.colorLightGrey,
+  },
+  completedText: {
+    color: theme.colorGrey,
+    textDecorationLine: "line-through",
+    textDecorationColor: theme.colorGrey,
+  },
+  completedButton: { backgroundColor: theme.colorGrey },
+});
