cf05d2526eedcbcfe14f5bd42565409b92889a24
Cristian Bucurenciu <bucurenciu.cristian@gmail.com>
2024-09-23 20:10:53 +0300

    feat: stack navigation

--- react-native/easy/taskly/app/index.tsx
+++ react-native/easy/taskly/app/index.tsx
@@ -1,10 +1,18 @@
 import { StyleSheet, View } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href={"/counter"}
+        style={{ textAlign: "center", fontSize: 24, marginBottom: 20 }}
+      >
+        {" "}
+        Go to /counter
+      </Link>
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" isCompleted />
       <ShoppingListItem name="Milk" isCompleted />
